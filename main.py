be08ce2f220daf92b1e7adeea6bd8486dc4e339a
Kevin Smith <kevin.ar.smith@gmail.com>
2021-02-07 11:00:54 -0600

    progress_4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
-file_path ="C:\Users\kevin\Desktop\KS-Homework\python-challenge\03-Python_Hwk\Instructions\PyBank\Resources\budget_data.csv"
-with open(file_path) as csvfile:
+file_csv ="C:\Users\kevin\Desktop\KS-Homework\python-challenge\03-Python_Hwk\Instructions\PyBank\Resources\budget_data.csv"
+with open(file_csv) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile)
     print(f" csvHeader:{csv_header}")
@@ -8,19 +8,16 @@ with open(file_path) as csvfile:
     csv_header = next(csvreader)
     
     # Create Variables
-    line_num = 0
-    profitloss=[]
-    profitlossdates=[]
-    profitlosssum=0
-    maximum_pl = 0
-    minimum_pl = 0
-    sum=0
+    total_months_1
+    pre_revenue=0
+    month_chg=[]
+    rev_chg = []
+    great_inc = ["",0]
+    great_dec = ["",0]
+    trevenue = 0
 
-    for row in csvreader
-    line_num +=1
-    profitloss.append(int(row[1]))
-    profitlosssum +- int(row[1])
-    profitlossdates.append(row[0])
+
+
 
 
 
