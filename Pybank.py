0fa83565065e71f0e45799acc6396eb8affcbd84
Kevin Smith <kevin.ar.smith@gmail.com>
2021-02-07 14:28:15 -0600

    lol

--- Pybank.py
+++
@@ -1,9 +0,0 @@
-file_path = "C:\Users\kevin\Desktop\KS-Homework\python-challenge\PyBank.csv"
-with open(file_path) as csvfile:
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile)
-    # Read the header row first(skip this step if there is no header)
-    csv_header = next(csvreader)
-    
-
-
