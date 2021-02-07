dd422fb10867d34b13c344100e334bfe82ea5a01
Kevin Smith <kevin.ar.smith@gmail.com>
2021-02-07 11:34:09 -0600

    progress_4

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
+# import csv
 import csv
+# file I need to load
 file_csv ="C:\Users\kevin\Desktop\KS-Homework\python-challenge\03-Python_Hwk\Instructions\PyBank\Resources\budget_data.csv"
+# Variables that I plan to work with
+Tomonth = 0
+prevrevenue = 0
+TOrevenue =0
+revenuechg =0
+revenue_chg =[]
+monthof_chg = []
+great_inc = ["",0]
+great_dec = ["",0]
+
+# read the appropirate file
 with open(file_csv) as csvfile:
-    # CSV reader specifies delimiter and variable that holds contents
+    # CSV reader specifies variable that holds contents
     csvreader = csv.reader(csvfile)
-    print(f" csvHeader:{csv_header}")
-    # Read the header row first(skip this step if there is no header)
+    print(f"csvHeader:{csv_header}")
+    # Read the header row first (skip it)
     csv_header = next(csvreader)
+
+# Loop through number sequence
+for row in reader   
+    Tomonth = Tomonth +1
+    TOrevenue = TOrevenue +int(row["Revenue"]) 
     
-    # Create Variables
-    total_months_1
-    pre_revenue=0
-    month_chg=[]
-    rev_chg = []
-    great_inc = ["",0]
-    great_dec = ["",0]
-    trevenue = 0
+
+    
+
 
 
 
