7c25e83758fe7d7ba4a5d19a630238a2757454a6
Kevin Smith <kevin.ar.smith@gmail.com>
2021-02-08 16:40:31 -0600

    checkpoint

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,14 +20,7 @@ file_path = "./Resources/election_data.csv"
 out_file = "./Analysis/output.txt"
 
 
-with open(file_path) as csvfile:
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile)
-    # Read the header row first(skip this step if there is no header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-    # Read each row of data after the header
-    for row in csvreader:
+
       
 
 
