38a98d062f0f99889b55feb62a417e7c6ca690d8
Kevin Smith <kevin.ar.smith@gmail.com>
2021-02-08 16:51:52 -0600

    test vote count

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,9 +5,9 @@ County = []
 Candidates =[]
 khan = []
 correy = []
-greatest_votes = ["",0}]
-C_options = []
-candidate_v = {}
+li = []
+otooley =[]
+
 
 with open(file_path) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
@@ -15,20 +15,20 @@ with open(file_path) as csvfile:
     # Read the header row first(skip this step if there is no header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-    
-
 file_path = "./Resources/election_data.csv"
 out_file = "./Analysis/output.txt"
-
-
 
 # Read each row of data after the header
     for row in csvreader:
         # The total number of votes cast (loop)
-        votes = votes + 1
-        t_candidates = row["Candidate"]
+        votes.append(int(row[0])
+        county.append(row[1])
+        candidates.append(row[2])
+        t_votes = (len(Votes))
+        print (t_votes)
 
-        if row[]
+
+        #if row[]
 
 # A complete list of candidates who received votes
 # The percentage of votes each candidate won
