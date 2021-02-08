d6f896e30954e6b2389dd887e84f39d4444917b3
Kevin Smith <kevin.ar.smith@gmail.com>
2021-02-08 12:39:34 -0600

    votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,26 @@
+import csv
+#The total number of  votes casr
+
+
+file_path = "./Resources/election_data.csv"
+out_file = "./Analysis/output.txt"
+
+
+with open(file_path) as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile)
+    # Read the header row first(skip this step if there is no header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    # Read each row of data after the header
+    for row in csvreader:
+      
+
+
+
+
+
+
 # The total number of votes cast
 # A complete list of candidates who received votes
 # The percentage of votes each candidate won
