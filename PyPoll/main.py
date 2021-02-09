df8780dc9f485f8cccf4ef59254f4071116e63f7
Kevin Smith <kevin.ar.smith@gmail.com>
2021-02-09 08:30:01 -0600

    KISS method redo

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,5 @@
 import csv
-#Define Variables
-votes = []
-count = []
-candidates = []
-khan = []
-correy = []
-li = []
-otooley = []
+
 
 file_path = "./Resources/election_data.csv"
 out_file = "./Analysis/output.txt"
@@ -19,25 +12,14 @@ with open(file_path) as csvfile:
     print(f"CSV Header: {csv_header}")
 # Read each row of data after the header
     for row in csvreader:
-    # append the data rows together
-    votes.append(int(row[0]))
-    county.append(row[1])
-    candidates.append(row[2])
-#Total number of votes
-total_votes = (len(votes))
-    for candidate in candidates
-    if candidate == "Khan":
-        khan.append(candidates)
-        khan_votes = len(khan)
-    elif candidate == "Correy":
-        correy.append
+    
 
 
 
 #print results
 print("ELection Results")
 print("----------------------")
-print(f"Total Votes: {total_count_votes}")
+print(f"Total Votes: {total_votes!T}")
 print("----------------------")
 for count in range(len(candidates)):
     print(f"{candidates[count]}: {percentage[count]}% ({vote_counts[count]})")
