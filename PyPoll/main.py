cf0169741eb2ca835805a83cb1335919edeec95b
Kevin Smith <kevin.ar.smith@gmail.com>
2021-02-09 10:14:50 -0600

    Nelson

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,30 +7,30 @@ correy = 0
 otooley = 0
 winner_count = 0
 #Generate a list of the candidates
-can_list = ["li", "kahn", "correy", "ottoley"]
+can_list = ["li", "kahn", "correy", "otooley"]
 #Set input and outfile
 file_path = "./Resources/election_data.csv"
 out_file = "./Analysis/output.txt"
 
 with open(file_path) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile,)
+    csvreader = csv.reader(csvfile)
     # Read the header row first(skip this step if there is no header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 # Read each row of data after the header
 for row in csvreader:
     votes = votes + 1 
-# Calculate votes for Li
-#    if (row[2]) == 'Li':
-#        li = li + 1
-# Calculate votes for Kahn    
-#    if (row[2]) == 'Khan':
-#        khan = khan + 1
-#   if (row[2]) == 'Correy':
-#        correy = correy + 1
-    #if (row[2]) == 'OTooley':
-    #    otooley = otooley + 1
+#Calculate votes for Li
+    if (row[2]) == 'Li':
+        li = li + 1
+#Calculate votes for Kahn    
+    if (row[2]) == 'Khan':
+        khan = khan + 1
+    if (row[2]) == 'Correy':
+        correy = correy + 1
+    if (row[2]) == 'OTooley':
+        otooley = otooley + 1
 
     
 
@@ -40,10 +40,10 @@ print("Election Results")
 print("----------------------")
 print(f"Total Votes: {votes}")
 print("----------------------")
-print(f"Li: {Li}")
-print(f"Khan: {Khan}")
-print(f"Correy: {Correy}")
-print(f"O'tooley: {Otooley}")
+print(f"Li: {li}")
+print(f"Khan: {khan}")
+print(f"Correy: {correy}")
+print(f"O'tooley: {otooley}")
 
 print("----------------------")
 
